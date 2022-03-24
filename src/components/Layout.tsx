cc5cd6370000ab4ca5996e376993d15bcb49b892
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-03-24 19:21:31 -0300

    feat: add Footer component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
-import { Container, Link, Stack, Text } from '@chakra-ui/react';
+import { Container, Stack } from '@chakra-ui/react';
 import { FC } from 'react';
 
-import { Header } from './';
+import { Footer, Header } from './';
 
 export const Layout: FC = ({ children }) => {
   return (
@@ -13,22 +13,7 @@ export const Layout: FC = ({ children }) => {
       <Container maxW='container.md' px={6} py={16}>
         <Stack mb={24}>{children}</Stack>
 
-        {/* TODO: move to Footer component */}
-        <footer>
-          <Stack>
-            <Text>
-              <Link
-                href='mailto:cryptography@ethereum.org'
-                textDecoration='underline'
-                color='gray.500'
-                _hover={{ color: '#667bbe' }}
-              >
-                cryptography@ethereum.org
-              </Link>
-            </Text>
-            <Text>{`© ${new Date().getFullYear()} Ethereum Foundation. All rights reserved.`}</Text>
-          </Stack>
-        </footer>
+        <Footer />
       </Container>
     </>
   );
