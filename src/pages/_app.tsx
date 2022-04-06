9f2724078b821fef2c6ceb216810eca1ab0c2cc2
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-04-06 20:29:41 -0300

    feat: add favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,13 +10,17 @@ import 'focus-visible/dist/focus-visible';
 
 import theme from '../theme';
 
+import favicon from '../../public/images/favicon.ico';
+import favicon16 from '../../public/images/favicon-16x16.png';
+import favicon32 from '../../public/images/favicon-32x32.png';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* <link rel='icon' type='image/x-icon' href={favicon.src} />
+        <link rel='icon' type='image/x-icon' href={favicon.src} />
         <link rel='icon' type='image/png' sizes='32x32' href={favicon32.src} />
-        <link rel='icon' type='image/png' sizes='16x16' href={favicon16.src} /> */}
+        <link rel='icon' type='image/png' sizes='16x16' href={favicon16.src} />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
 
