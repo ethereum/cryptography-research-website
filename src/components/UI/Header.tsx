e3268f08c0d7b1539f94f3caf22b903adb84518a
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-04-06 18:49:20 -0300

    chore: update logo SVG

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 
-import EFlogo from '../../../public/images/ef-logo.png';
+import EFlogo from '../../../public/images/ef-logo.svg';
 
 import { HOME_URL } from '../../constants';
 import { Nav } from '../Nav';
@@ -16,7 +16,7 @@ export const Header: FC = () => {
       <Container maxW={{ base: 'container.xl2', xl2: 'container.xl' }}>
         <Flex justifyContent='space-between' px={{ base: 6, md: 8 }} pt={6}>
           <Box mr={{ base: 8, md: 0 }} onClick={() => router.push(HOME_URL)} cursor='pointer'>
-            <Image src={EFlogo} alt='Ethereum Foundation logo' height={50} width={146.66} />
+            <Image src={EFlogo} alt='Ethereum Foundation logo' height={55} width={161} />
           </Box>
 
           <Nav />
