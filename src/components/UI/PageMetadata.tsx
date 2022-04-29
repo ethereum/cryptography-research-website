88d84dfcc5f0d50395bd1ac8febff62b2e1a8266
Sam Richards <sbrichards@gmail.com>
2022-04-29 12:45:58 +0200

    Replace meta image

--- src/components/UI/PageMetadata.tsx
+++ src/components/UI/PageMetadata.tsx
@@ -17,21 +17,21 @@ export const PageMetadata: FC<Props> = ({ title, description }) => {
       <meta name='title' content={`${title} | ${HEAD_TITLE}`} />
       <meta name='description' content={description} />
       <meta name='application-name' content={HEAD_TITLE} />
-      <meta name='image' content='https://crypto.ethereum.org/images/ef-logo-white-bg.png' />
+      <meta name='image' content='https://crypto.ethereum.org/images/ef-logo-bg-white.png' />
       {/* OpenGraph */}
       <meta property='og:title' content={`${title} | ${HEAD_TITLE}`} />
       <meta property='og:description' content={description} />
       <meta property='og:type' content='website' />
       <meta property='og:site_name' content={HEAD_TITLE}></meta>
       <meta property='og:url' content='https://crypto.ethereum.org/' />
-      <meta property='og:image' content='https://crypto.ethereum.org/images/ef-logo-white-bg.png' />
+      <meta property='og:image' content='https://crypto.ethereum.org/images/ef-logo-bg-white.png' />
       <meta
         property='og:image:url'
-        content='https://crypto.ethereum.org/images/ef-logo-white-bg.png'
+        content='https://crypto.ethereum.org/images/ef-logo-bg-white.png'
       />
       <meta
         property='og:image:secure_url'
-        content='https://crypto.ethereum.org/images/ef-logo-white-bg.png'
+        content='https://crypto.ethereum.org/images/ef-logo-bg-white.png'
       />
       <meta property='og:image:alt' content={HEAD_TITLE} />
       <meta property='og:image:type' content='image/png' />
@@ -44,7 +44,7 @@ export const PageMetadata: FC<Props> = ({ title, description }) => {
       <meta name='twitter:description' content={description} />
       <meta
         name='twitter:image'
-        content='https://crypto.ethereum.org/images/ef-logo-white-bg.png'
+        content='https://crypto.ethereum.org/images/ef-logo-bg-white.png'
       />
     </Head>
   );
