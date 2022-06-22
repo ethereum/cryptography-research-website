b96039c5601e8556481be8ede042340ac415b5a8
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-06-22 12:43:07 -0300

    feat: add ZK Hash bounties page

---
+++ src/pages/bounties/zk-hash.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/no-children-prop */
+import { Heading } from '@chakra-ui/react';
+import type { GetStaticProps, NextPage } from 'next';
+import fs from 'fs';
+import matter from 'gray-matter';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import rehypeRaw from 'rehype-raw';
+import rehypeSlug from 'rehype-slug';
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
+
+import { PageMetadata } from '../../components/UI';
+import { PostTheme } from '../../components/styles';
+
+import { ZK_HASH_DATA_SOURCE } from '../../constants';
+
+// generate the static props for the page
+export const getStaticProps: GetStaticProps = async () => {
+  const fileName = fs.readFileSync(ZK_HASH_DATA_SOURCE, 'utf-8');
+  const { content } = matter(fileName);
+
+  return {
+    props: {
+      content
+    }
+  };
+};
+
+interface Props {
+  content: string;
+}
+
+const ZKHash: NextPage<Props> = ({ content }) => {
+  return (
+    <>
+      <PageMetadata
+        title='ZK Hash Function Cryptanalysis bounties'
+        description='Help us understand the security of new hash functions better.'
+      />
+
+      <main>
+        <Heading as='h1' mb={20}>
+          ZK Hash Function Cryptanalysis bounties
+        </Heading>
+
+        <ReactMarkdown
+          components={ChakraUIRenderer(PostTheme)}
+          children={content}
+          remarkPlugins={[gfm, remarkMath]}
+          rehypePlugins={[rehypeKatex, rehypeRaw, rehypeSlug]}
+        />
+      </main>
+    </>
+  );
+};
+
+export default ZKHash;
