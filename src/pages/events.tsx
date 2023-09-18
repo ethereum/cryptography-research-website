f7ad6eb081931a9ea94879876ab6aff1b1faaf93
Dmitry Khovratovich <khovratovich@gmail.com>
2023-09-18 14:43:34 +0200

    Update events.tsx
    VDF event added

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -17,6 +17,15 @@ const Events: NextPage = () => {
         </Heading>
 
         <Stack mb={6} spacing={8}>
+             <Event
+            conference='MinRoot VDF event 2023, Lyon'
+            workshop='Analysis of candidate sequential function MinRoot'
+          >
+           Between April 28th and 30th, 2023, the Ethereum Foundation invited a group of researchers to conduct an initial analysis of the candidate sequential function MinRoot. 
+               The purpose of this gathering was to collectively delve into the intricacies of MinRoot and assess its potential implications for the Ethereum ecosystem. 
+               Preliminary analysis was conducted by Gaetan Leurent, Maria Naya Plasencia, and Stefano Tessaro. At the event, the researchers were divided into three groups,
+               each tasked with different aspects of MinRoot's evaluation. The resulting report serves as a comprehensive joint summary, presenting the culmination of the researchers' intensive efforts during the event.
+          </Event>
           <Event
             conference='Cryptographic Frontier 2022, Trondheim'
             workshop='Open Problems in Decentralized Computation at Eurocrypt 2022'
